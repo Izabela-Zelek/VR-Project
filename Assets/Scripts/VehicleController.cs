5ec9bf3b89306db29b49458bbaf1c227e2b699e4
Izabela-Zelek <c00247865@itcarlow.ie>
2023-03-30 13:50:26 +0100

    Finished boundary and road layout, added cars and car movement

---
+++ Assets/Scripts/VehicleController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleController : MonoBehaviour
+{
+    private int _roadChoice;
+    private int _nextSpawn;
+    private Object[] _carTypes;
+    private GameObject _chosenCar;
+    private GameObject _newCar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _carTypes = Resources.LoadAll("Vehicles", typeof(GameObject));
+        _nextSpawn = Random.Range(3, 11);
+        _roadChoice = Random.Range(0, 6);
+        _chosenCar = (GameObject)_carTypes[Random.Range(0, _carTypes.Length)];
+
+        StartCoroutine(WaitForSpawn());
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.collider.name);
+
+    }
+    private IEnumerator WaitForSpawn()
+    {
+        while (true)
+        {
+            Vector3 startPos = new Vector3();
+            Quaternion rot = new Quaternion();
+            if(_roadChoice == 0 || _roadChoice == 1)
+            {
+                startPos = GameObject.Find("Entr1").transform.position;
+                rot = GameObject.Find("Entr1").transform.rotation;
+            }
+            else if(_roadChoice == 2 || _roadChoice == 3)
+            {
+                startPos = GameObject.Find("Entr2").transform.position;
+                rot = GameObject.Find("Entr2").transform.rotation;
+            }
+            else
+            {
+                startPos = GameObject.Find("Entr3").transform.position;
+                rot = GameObject.Find("Entr3").transform.rotation;
+            }
+            _newCar = Instantiate(_chosenCar,new Vector3(startPos.x, startPos.y + _chosenCar.transform.position.y,startPos.z), rot, this.transform.GetChild(3).transform) as GameObject;
+            _newCar.GetComponent<VehicleMover>().SetPointsByChildren(transform.GetChild(2).GetChild(_roadChoice).gameObject);
+            _nextSpawn = Random.Range(3, 11);
+            _roadChoice = Random.Range(0, 6);
+            _chosenCar = (GameObject)_carTypes[Random.Range(0, _carTypes.Length)];
+
+            yield return new WaitForSeconds(_nextSpawn);
+        }
+    }
+}
---
+++ Assets/Scripts/VehicleMover.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class VehicleMover : MonoBehaviour
+{
+    public List<Vector3> path;
+    public float speed = 5.0f;
+    public float mass = 5.0f;
+    public float maxSteer = 15.0f;
+    public float pathRadius = 1.0f;
+
+    public int currentWaypointIndex = 0;
+    private Vector3 targetWaypoint;
+    private Vector3 desiredVelocity;
+    private Vector3 steeringForce;
+    private Rigidbody rb;
+    private float yPos;
+    private bool pathForward = true;
+    public int id = -1;
+    private Vector3 previousPosition;
+    void Start()
+    {
+
+        if (id == -1)
+        {
+            id = Random.Range(0, 10000);
+        }
+        rb = GetComponent<Rigidbody>();
+        yPos = transform.localPosition.y;
+        previousPosition = new Vector3(0,0,0);
+
+    }
+
+    private void SetPoints(IEnumerable<Vector3> points)
+    {
+        path = (List<Vector3>)points;
+        targetWaypoint = GetClosestPointOnPath(transform.position);
+    }
+
+    public void SetPointsByChildren(GameObject parent)
+    {
+        for (int i = 0; i < parent.transform.childCount; i++)
+        {
+            path.Add(parent.transform.GetChild(i).transform.position);
+        }
+
+        targetWaypoint = GetClosestPointOnPath(transform.position);
+    }
+
+    void Update()
+    {
+        if (path.Count > 0)
+        {
+            float distance = Vector3.Distance(transform.position, targetWaypoint);
+
+            if (distance <= pathRadius)
+            {
+                currentWaypointIndex++;
+
+                if (currentWaypointIndex >= path.Count && pathForward)
+                {
+                    Destroy(this.gameObject);
+                }
+                targetWaypoint = path[currentWaypointIndex];
+            }
+
+            desiredVelocity = (targetWaypoint - transform.position).normalized * speed;
+            steeringForce = desiredVelocity - rb.velocity;
+            steeringForce /= mass;
+
+            if (steeringForce.magnitude > maxSteer)
+            {
+                steeringForce = steeringForce.normalized * maxSteer;
+            }
+
+            rb.AddForce(steeringForce);
+            Vector3 vel = new Vector3(rb.velocity.x, 0, rb.velocity.z); 
+            Debug.Log(vel.x);
+            Debug.Log(vel.y);
+            Debug.Log(vel.z);
+
+            transform.rotation = Quaternion.LookRotation(vel);
+            transform.rotation *= Quaternion.Euler(0f, -90f, 0f);
+            transform.localPosition = new Vector3(transform.localPosition.x, yPos, transform.localPosition.z);
+        }
+    }
+
+    private Vector3 GetClosestPointOnPath(Vector3 position)
+    {
+        Vector3 closestPoint = Vector3.zero;
+        float closestDistance = Mathf.Infinity;
+
+        for (int i = 0; i < path.Count; i++)
+        {
+            Vector3 pathPoint = path[i];
+            float distance = Vector3.Distance(position, pathPoint);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestPoint = pathPoint;
+                currentWaypointIndex = i;
+            }
+        }
+
+        return closestPoint;
+    }
+}
